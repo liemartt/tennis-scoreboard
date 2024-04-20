8b5c7a8846f5ed4a36949612917e4d8ba3a9699c
Artem_Limonov <liemartt@yandex.ru>
2024-04-20 21:19:42 +0300

    feat: add MatchScoreCalculationService

--- src/main/java/com/liemartt/service/MatchScoreCalculationService.java
+++ src/main/java/com/liemartt/service/MatchScoreCalculationService.java
@@ -1,11 +1,74 @@
 package com.liemartt.service;
 
+import com.liemartt.model.PLayerScore;
 import com.liemartt.model.MatchScore;
+import com.liemartt.model.Player;
 
 public class MatchScoreCalculationService {
-    MatchScore matchScore;
+    private final MatchScore matchScore;
+    private PLayerScore ally;
+    private PLayerScore enemy;
 
     public MatchScoreCalculationService(MatchScore matchScore) {
         this.matchScore = matchScore;
     }
+
+    public void addPointToPlayer(Player player) {
+        if (player.equals(getPlayer1())) {
+            ally = matchScore.getPlayer1();
+            enemy = matchScore.getPlayer2();
+        } else {
+            enemy = matchScore.getPlayer1();
+            ally = matchScore.getPlayer2();
+        }
+        if (matchScore.isTieBreak()) {
+            addTieBreakPoint();
+            return;
+        }
+
+        if (ally.getPointCounter() <= 2) ally.addPoint();
+        else if (ally.getPointCounter() == 3 && enemy.getPointCounter() <= 2) {
+            addGameToAlly();
+        } else if (ally.getPointCounter() == 3 && ally.isAdvantage()) {
+            addGameToAlly();
+        } else {
+            if (enemy.isAdvantage()) enemy.setAdvantage(false);
+            else ally.setAdvantage(true);
+        }
+    }
+
+    private void addGameToAlly() {
+        ally.addGame();
+        ally.resetPointCounter();
+        enemy.resetPointCounter();
+
+        if (ally.getGameCounter() == 6 && enemy.getGameCounter() == 6) {
+            matchScore.setTieBreak(true);
+        } else if (ally.getGameCounter() == 7 && enemy.getGameCounter() <= 5) {
+            addSetToAlly();
+        }
+    }
+
+    private void addTieBreakPoint() {
+        ally.addTieBreak();
+        if (ally.getTieBreaksCounter() == 7) {
+            matchScore.setTieBreak(false);
+            addSetToAlly();
+        }
+    }
+
+    private void addSetToAlly() {
+        ally.addSet();
+        ally.resetGameCounter();
+        enemy.resetGameCounter();
+        if (ally.getSetCounter() == 2) {
+            matchScore.setFinished(true);
+            matchScore.setWinner(ally);
+        }
+    }
+
+    private Player getPlayer1() {
+        return matchScore.getPlayer1().getPlayer();
+    }
+
 }
