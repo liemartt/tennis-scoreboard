07e09140169c1a5d0f213d2d36036eb42db414d9
Artem_Limonov <liemartt@yandex.ru>
2024-04-20 22:25:41 +0300

    feat: update servlet for check if names are unique

--- src/main/java/com/liemartt/servlet/NewMatchServlet.java
+++ src/main/java/com/liemartt/servlet/NewMatchServlet.java
@@ -1,5 +1,10 @@
 package com.liemartt.servlet;
 
+import com.liemartt.dao.PlayerDAO;
+import com.liemartt.dao.PlayerDAOImpl;
+import com.liemartt.model.Match;
+import com.liemartt.model.Player;
+import com.liemartt.service.OngoingMatchesService;
 import com.liemartt.util.ThymeleafUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -11,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
 
 @WebServlet(urlPatterns = "/new-match")
 public class NewMatchServlet extends HttpServlet {
@@ -21,12 +28,21 @@ public class NewMatchServlet extends HttpServlet {
         WebContext context = ThymeleafUtil.getWebContext(req, resp, servletContext);
         templateEngine.process("new-match", context, resp.getWriter());
     }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PlayerDAO playerDAO = new PlayerDAOImpl();
         ServletContext servletContext = getServletContext();
         TemplateEngine templateEngine = ThymeleafUtil.getTemplateEngine(servletContext);
         WebContext context = ThymeleafUtil.getWebContext(req, resp, servletContext);
+
         String firstPlayerName = req.getParameter("player1_name");
         String secondPlayerName = req.getParameter("player2_name");
+        Player firstPlayer = playerDAO.getPlayerByName(firstPlayerName).orElseGet(() -> playerDAO.addNewPlayer(firstPlayerName));
+        Player secondPlayer = playerDAO.getPlayerByName(secondPlayerName).orElseGet(() -> playerDAO.addNewPlayer(secondPlayerName));
+        //TODO check players
+        Match match = new Match(firstPlayer, secondPlayer);
+        UUID uuid = OngoingMatchesService.addNewMatch(match);
+        System.out.println(uuid);
     }
 }
