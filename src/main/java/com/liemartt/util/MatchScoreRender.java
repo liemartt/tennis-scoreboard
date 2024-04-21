6f1f9e26c086f70d3f8a01d78dd8669a7292016d
Artem_Limonov <liemartt@yandex.ru>
2024-04-21 14:21:11 +0300

    fix: change points to tennis points (0,15,30,40)

--- src/main/java/com/liemartt/util/MatchScoreRender.java
+++ src/main/java/com/liemartt/util/MatchScoreRender.java
@@ -16,9 +16,31 @@ public class MatchScoreRender {
         context.setVariable("player2Sets", matchScore.getPlayer2().getSetCounter());
         context.setVariable("player1Games", matchScore.getPlayer1().getGameCounter());
         context.setVariable("player2Games", matchScore.getPlayer2().getGameCounter());
-        context.setVariable("player1Points", matchScore.getPlayer1().getPointCounter());
-        context.setVariable("player2Points", matchScore.getPlayer2().getPointCounter());
+
         context.setVariable("player1TieBreaks", matchScore.getPlayer1().getTieBreaksCounter());
         context.setVariable("player2TieBreaks", matchScore.getPlayer2().getTieBreaksCounter());
+        context.setVariable("player1Advantage", matchScore.getPlayer1().isAdvantage());
+        context.setVariable("player2Advantage", matchScore.getPlayer2().isAdvantage());
+
+
+        int player1Points = getRealPoints(matchScore.getPlayer1().getPointCounter());
+        int player2Points = getRealPoints(matchScore.getPlayer2().getPointCounter());
+        context.setVariable("player1Points", player1Points);
+        context.setVariable("player2Points", player2Points);
+
+
+    }
+    private static int getRealPoints(int player1Points){
+        switch (player1Points){
+            case 0:
+                return 0;
+            case 1:
+                return 15;
+            case 2:
+                return 30;
+            case 3:
+                return 40;
+        }
+        return 0;
     }
 }
