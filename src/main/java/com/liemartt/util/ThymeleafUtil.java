accf74c6f78b9bcf0fbc2448bf51cb4fdc1e23df
Artem_Limonov <liemartt@yandex.ru>
2024-07-04 10:12:51 +0300

    chore: refactor

--- src/main/java/com/liemartt/util/ThymeleafUtil.java
+++ src/main/java/com/liemartt/util/ThymeleafUtil.java
@@ -1,5 +1,10 @@
 package com.liemartt.util;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -7,15 +12,22 @@ import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-
-import java.util.Locale;
-
 @WebListener
 public class ThymeleafUtil implements ServletContextListener {
+
+    private static final String TEMPLATE_ENGINE_ATTR = "templateEngine";
+
+    public static void storeTemplateEngine(ServletContext context, TemplateEngine engine) {
+        context.setAttribute(TEMPLATE_ENGINE_ATTR, engine);
+    }
+
+    public static TemplateEngine getTemplateEngine(ServletContext context) {
+        return (TemplateEngine) context.getAttribute(TEMPLATE_ENGINE_ATTR);
+    }
+
+    public static WebContext getWebContext(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext) {
+        return new WebContext(req, resp, servletContext, req.getLocale());
+    }
 
     public void contextInitialized(ServletContextEvent sce) {
         TemplateEngine engine = templateEngine(sce.getServletContext());
@@ -37,18 +49,6 @@ public class ThymeleafUtil implements ServletContextListener {
         resolver.setSuffix(".html");
         resolver.setCharacterEncoding("UTF-8");
         return resolver;
-    }
-    private static final String TEMPLATE_ENGINE_ATTR = "templateEngine";
-
-    public static void storeTemplateEngine(ServletContext context, TemplateEngine engine) {
-        context.setAttribute(TEMPLATE_ENGINE_ATTR, engine);
-    }
-
-    public static TemplateEngine getTemplateEngine(ServletContext context) {
-        return (TemplateEngine) context.getAttribute(TEMPLATE_ENGINE_ATTR);
-    }
-    public static WebContext getWebContext(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext){
-        return new WebContext(req, resp, servletContext, req.getLocale());
     }
 
 }
