cb63077a1c83193a998639953e3ba5cba6ad10ab
Artem_Limonov <liemartt@yandex.ru>
2024-07-31 19:40:52 +0300

    fix

--- src/test/java/com/liemartt/MatchScoreCalculationServiceTest.java
+++ src/test/java/com/liemartt/MatchScoreCalculationServiceTest.java
@@ -1,117 +1,144 @@
 package com.liemartt;
 
-import com.liemartt.model.Match;
 import com.liemartt.model.MatchScore;
 import com.liemartt.model.Player;
 import com.liemartt.service.MatchScoreCalculationService;
+import com.liemartt.service.handler_chain.ChainBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MatchScoreCalculationServiceTest {
+    MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService(ChainBuilder.buildChain());
+
     @Test
     public void gameNotEndedWhenPlayer1ScoresAt40_40() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 3; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-        Assertions.assertEquals(matchScore.getFirstPlayerScore().getGameCounter(), 0);
-        Assertions.assertEquals(matchScore.getSecondPlayerScore().getGameCounter(), 0);
+        matchScore.setPointWinner(matchScore.getPlayer1());
+        matchScoreCalculationService.execute(matchScore);
+        Assertions.assertEquals(matchScore.getPlayer1Score().getGameCounter(), 0);
+        Assertions.assertEquals(matchScore.getPlayer2Score().getGameCounter(), 0);
     }
 
     @Test
     public void gameEndedWhenPlayer1ScoresAt40_0() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 3; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-        Assertions.assertEquals(matchScore.getFirstPlayerScore().getGameCounter(), 1);
-        Assertions.assertEquals(matchScore.getSecondPlayerScore().getGameCounter(), 0);
+        matchScoreCalculationService.execute(matchScore);
+        Assertions.assertEquals(matchScore.getPlayer1Score().getGameCounter(), 1);
+        Assertions.assertEquals(matchScore.getPlayer2Score().getGameCounter(), 0);
     }
+
     @Test
     public void gameSwitchesToTieBreakAt6_6() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 20; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 24; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 4; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
         Assertions.assertTrue(matchScore.isTieBreak());
     }
+
     @Test
     public void playerWinsMatchByWinningTwoSets() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 54; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
-        Assertions.assertEquals(matchScore.getWinner(), matchScore.getFirstPlayerScore());
+        Assertions.assertEquals(matchScore.getWinner(), matchScore.getPlayer1Score());
     }
+
     @Test
     public void playerWinsSetAtScore7_5() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 20; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 20; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 8; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
-        Assertions.assertEquals(matchScore.getFirstPlayerScore().getSetCounter(), 1);
+        Assertions.assertEquals(matchScore.getPlayer1Score().getSetCounter(), 1);
     }
+
     @Test
     public void playerLosesAtScore40_40WithAdvantage() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 3; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+        matchScore.setPointWinner(matchScore.getPlayer1());
+        matchScoreCalculationService.execute(matchScore);
 
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+        matchScore.setPointWinner(matchScore.getPlayer2());
+        matchScoreCalculationService.execute(matchScore);
+        matchScoreCalculationService.execute(matchScore);
+        matchScoreCalculationService.execute(matchScore);
 
 
-        Assertions.assertEquals(matchScore.getSecondPlayerScore().getGameCounter(), 1);
+        Assertions.assertEquals(matchScore.getPlayer2Score().getGameCounter(), 1);
     }
+
     @Test
     public void playerWinsTieBreakAtScore7_5() {
         Player player1 = new Player("firstPLayer");
         Player player2 = new Player("secondPLayer");
-        MatchScore matchScore = new MatchScore(new Match(player1, player2));
+        MatchScore matchScore = new MatchScore(player1, player2);
         for (int i = 0; i < 24; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 24; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
         for (int i = 0; i < 5; i++) {
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-            MatchScoreCalculationService.addPointToPlayer(matchScore, player2);
+            matchScore.setPointWinner(matchScore.getPlayer1());
+            matchScoreCalculationService.execute(matchScore);
+            matchScore.setPointWinner(matchScore.getPlayer2());
+            matchScoreCalculationService.execute(matchScore);
         }
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
-        MatchScoreCalculationService.addPointToPlayer(matchScore, player1);
+        matchScore.setPointWinner(matchScore.getPlayer1());
+        matchScoreCalculationService.execute(matchScore);
+        matchScoreCalculationService.execute(matchScore);
 
-        Assertions.assertEquals(matchScore.getFirstPlayerScore().getSetCounter(), 1);
+        Assertions.assertEquals(matchScore.getPlayer1Score().getSetCounter(), 1);
     }
 }
